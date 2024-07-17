6aa7f4877f4de67f52ba3409aa062bf0c8aa3a5c
farzamyousuf202 <90801520+farzamyousuf202@users.noreply.github.com>
2024-07-17 19:00:48 +0500

    Add files via upload

---
+++ index.ts
@@ -0,0 +1,32 @@
+#! /usr/bin/env  node
+
+
+// src/index.ts
+// src/index.ts
+import inquirer from 'inquirer';
+
+async function startGame() {
+  const questions = [
+    {
+      type: 'list',
+      name: 'choice',
+      message: 'You are in a dark forest. Which way do you go?',
+      choices: ['Left', 'Right', 'Forward']
+    }
+  ];
+
+  const answer = await inquirer.prompt(questions);
+
+  if (answer.choice === 'Left') {
+    console.log('You encounter a wild animal and run away!');
+  } else if (answer.choice === 'Right') {
+    console.log('You find a hidden treasure chest!');
+  } else {
+    console.log('You walk into a clearing and find a peaceful village.');
+  }
+
+  console.log('Game over. Thanks for playing!');
+}
+
+startGame();
+
